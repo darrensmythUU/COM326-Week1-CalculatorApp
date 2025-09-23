bc2f5ad9691818fc2f26dc352ce0da0c26a4af23
Kiphea <djsmyth05@gmail.com>
2025-09-23 19:06:12 +0100

    Inputting code for the calculator app

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Variable names should be short yet meaningful
+// The choice of a variable name should be mnemonic that is -
+// designed to indicate to the casual observer the intent of its use.
+
+// The first integer variable stores the first number
+int firstNumber = 5;
+
+// The second integer variable stores the second number
+int secondNumber = 20;
+
+// The "result" variable contains the addition
+// of firstNumber and secondNumber
+// Note that the integer type is used for "result" variable
+int result = firstNumber + secondNumber;
+Console.WriteLine("The addition of both numbers is {0}", result);
+Console.ReadKey();
