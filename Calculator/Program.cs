e26c3d5087ced7051a34a2f383514f8370bef28c
Kiphea <djsmyth05@gmail.com>
2025-09-24 14:35:50 +0100

    Introduction

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,17 +1,19 @@
-﻿// Variable names should be short yet meaningful
-// The choice of a variable name should be mnemonic that is -
-// designed to indicate to the casual observer the intent of its use.
+﻿CalculatorApp();
 
-// The integer variable stores the first number
-// Ask the user to input the first number
-Console.WriteLine("Type in the first number followed by the Enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+void CalculatorApp()
+{
+   // Ask the user to input the first number
+    Console.WriteLine("Type in the first number followed by the Enter key");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-// Ask the user to input the second number
-Console.WriteLine("Type in the second number followed by the Enter key");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+    // Ask the user to input the second number
+    Console.WriteLine("Type in the second number followed by the Enter key");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-// Perform the calculation
-int result = firstNumber + secondNumber;
-Console.WriteLine("Adding {0} and {1} gave the answer {2}", firstNumber, secondNumber, result);
-Console.ReadKey();
+    // Perform the calculation
+    int result = firstNumber + secondNumber;
+
+    // Output the answer to the console
+    Console.WriteLine("Adding {0} and {1} gave the answer {2}", firstNumber, secondNumber, result);
+    Console.ReadKey(); 
+}
